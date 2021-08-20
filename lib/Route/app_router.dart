8d8fdbdc8a4e7a2caa237b337423dbae498ada85
sifat hassan <sifat007.sh7@gmail.com>
2021-08-20 19:34:55 +0600

    stractured done

--- lib/Route/app_router.dart
+++ lib/Route/app_router.dart
@@ -1,27 +1,25 @@
+import 'package:fieldx/Bloc/test_cubit.dart';
 import 'package:fieldx/Constants/Strings/appStrings.dart';
+import 'package:fieldx/Presentation/Page/Home/HomePage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-// class AppRouter {
-//
-//   AppRouter() {
-//
-//   }
-//
-//   Route generateRoute(RouteSettings settings) {
-//     final arguments = settings.arguments as Map;
-//     switch (settings.name) {
-//       case "/":
-//         return MaterialPageRoute(builder: (_) => SplashScreen());
-//       case HOME_PAGE:
-//         return MaterialPageRoute(
-//             builder: (BuildContext context) => BlocProvider(
-//               create: (context) => UserCubit(),
-//               child: HomePage(),
-//             ));
-//
-//       default:
-//         return null;
-//     }
-//   }
-// }
+class AppRouter {
+  Route? generateRoute(RouteSettings settings) {
+    final arguments = settings.arguments as Map;
+    switch (settings.name) {
+      case "/":
+        return MaterialPageRoute(builder: (_) => HomePage());
+      case HOME_PAGE:
+        return MaterialPageRoute(
+            builder: (BuildContext context) => BlocProvider(
+              create: (context) => TestCubit(),
+              child: HomePage(),
+            ));
+
+      default:
+        return null;
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,50 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_literals_to_create_immutables
 
-void main() {
-  runApp(const MyApp());
+import 'package:fieldx/Bloc/test_cubit.dart';
+import 'package:fieldx/Route/app_router.dart';
+// @dart=2.9
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
+
+
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIOverlays([]);
+  HydratedBloc.storage=await HydratedStorage.build(
+    storageDirectory: await getApplicationDocumentsDirectory(),
+  );
+  runApp(MyApp(
+    router: AppRouter(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final AppRouter router;
+   const MyApp({Key? key,  required this.router}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<TestCubit>(
+          create: (context) => TestCubit(),
         ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch:Colors.deepOrange,
+          // primaryColor: Color.fromARGB(255, 133, 4, 224),
+        ),
+        onGenerateRoute: router.generateRoute,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
