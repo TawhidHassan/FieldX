1c29e0a063cc8a43270eddb1d2656b594205df75
sifat hassan <sifat007.sh7@gmail.com>
2021-08-20 14:13:40 +0600

    ready folder and packaage

---
+++ lib/Constants/Strings/appStrings.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+
+
+//base url of api
+const BASE_URL = "https://salesx.herokuapp.com/api/v1/";
+
+//route name
+const HOME_PAGE = "/home_page";
+const LOGIN_PAGE = "/login_page";
---
+++ lib/Route/app_router.dart
@@ -0,0 +1,27 @@
+import 'package:fieldx/Constants/Strings/appStrings.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+// class AppRouter {
+//
+//   AppRouter() {
+//
+//   }
+//
+//   Route generateRoute(RouteSettings settings) {
+//     final arguments = settings.arguments as Map;
+//     switch (settings.name) {
+//       case "/":
+//         return MaterialPageRoute(builder: (_) => SplashScreen());
+//       case HOME_PAGE:
+//         return MaterialPageRoute(
+//             builder: (BuildContext context) => BlocProvider(
+//               create: (context) => UserCubit(),
+//               child: HomePage(),
+//             ));
+//
+//       default:
+//         return null;
+//     }
+//   }
+// }
